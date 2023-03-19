aa62b9a178aa03767aaa1faff1a89dbacffb19ed
rocioalbarracin <122891650+rocioalbarracin@users.noreply.github.com>
2023-03-19 14:23:34 -0300

    Add files via upload

---
+++ funcionCV.js
@@ -0,0 +1,20 @@
+
+  
+function mostrarDireccion() {
+    document.getElementById("dir").innerHTML = "Open Door, Provincia Bs. As.";
+}
+function mostrarTelefono() {
+    document.getElementById("tel").innerHTML = "2323655516";
+}
+function mostrarEmail() {
+    document.getElementById("mail").innerHTML = "rocio_albarracin@yahoo.com.ar";
+}
+
+document.getElementById('mail').addEventListener('click',function(){
+    document.getElementById('mensaje').style.webkitTextFillColor='#FF0000';
+    document.getElementById('enviado').addEventListener('click', function(){
+    });
+});
+
+
+
